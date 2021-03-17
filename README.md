e352b61a5b8d808cef6a4e9367ebdf0bfd86eca5
张卓 <mycinbrin@gmail.com>
2021-03-17 18:01:57 +0800

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+# What is this?
+
+I want to run a c++ program on Apple M1 Rosetta 2. This program does not use instructions like AVX, but it still fails. 
+
+```
+# it will output "Illegal instruction: 4"
+run_mac_m1.sh
+```
--- run_mac_m1.sh
+++ run_mac_m1.sh
@@ -1,8 +1,10 @@
 # brew install nasm gmp 
-nasm -fmacho64 --prefix _  fr.asm
+arch -x86_64 /usr/local/bin/nasm -fmacho64 --prefix _  fr.asm
+#nasm -fmacho64 --prefix _  fr.asm
 # arch -x86_64 ? which compiler ? g++ vs clang++ ? system compiler or brew-installed compiler?
-# openmp can also be disabled
-arch -x86_64 g++ -pthread main.cpp calcwit.cpp utils.cpp fr.cpp fr.o circuit.c -o circuit -lgmp -std=c++11 -O3 -fopenmp -DSANITY_CHECK -I/usr/local/include -L/usr/local/lib/
+# TODO: -fopenmp
+arch -x86_64 /usr/local/bin/g++-10 -pthread main.cpp calcwit.cpp utils.cpp fr.cpp fr.o circuit.c -o circuit -lgmp -std=c++11 -O1 -DSANITY_CHECK -I/usr/local/include -L/usr/local/lib/ -march=x86-64
+# Illegal instruction: 4
 ./circuit input.json output.json
 if ! diff output.json witness.json
 then
